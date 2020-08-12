33f237e115ca21bbf818f7023d1e0e6c2f24b547
Jane02294 <46182833+Jane02294@users.noreply.github.com>
2020-08-12 22:24:29 +0700

    Add 6.2 Collect and display the data  : Step 5

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -91,5 +91,16 @@ class SleepTrackerViewModel(
             database.update(night)
         }
     }
+    fun onClear() {
+        uiScope.launch {
+            clear()
+            tonight.value = null
+        }
+    }
+    suspend fun clear() {
+        withContext(Dispatchers.IO) {
+            database.clear()
+        }
+    }
 }
 
