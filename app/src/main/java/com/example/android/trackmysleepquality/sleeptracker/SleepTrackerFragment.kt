00514099eb6147b8ffa8a2c2177cd9e07a302da0
Jane02294 <46182833+Jane02294@users.noreply.github.com>
2020-08-12 21:36:48 +0700

    Add 6.2 Add a ViewModel  : Step 4

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -49,9 +49,10 @@ class SleepTrackerFragment : Fragment() {
         val application = requireNotNull(this.activity).application
         val dataSource = SleepDatabase.getInstance(application).sleepDatabaseDao
         val viewModelFactory = SleepTrackerViewModelFactory(dataSource, application)
-        val sleepTrackerViewModel =
-        ViewModelProviders.of(
-                this, viewModelFactory).get(SleepTrackerViewModel::class.java)
+        val sleepTrackerViewModel = ViewModelProviders.of (this, viewModelFactory).get(SleepTrackerViewModel::class.java)
+
+        binding.setLifecycleOwner(this)
+        binding.sleepTrackerViewModel = sleepTrackerViewModel
 
         return binding.root
     }
